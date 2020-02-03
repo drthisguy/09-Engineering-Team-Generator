affbca23b71bb30b672a093dbb757367d45a9213
drthisguy <page.c.tyler@gmail.com>
2020-02-02 22:08:43 -0500

    prompt to add new employee? --> prompt for employee type

--- app.js
+++ app.js
@@ -13,16 +13,56 @@ const inquirer = require("inquirer"),
 })().then( ({ready}) => ready ? newTeam() : console.log('Okay. we\'ll wait than...'))
 .catch( err => console.log(err)) 
 
-function newTeam() {
- promptUser().then( input => {
-    promptMan().then(({office}) => {
-    const m = new Manager(input.name, input.id, input.email, office);
-    console.log(`New manager: "${m.name}" has been created.`);
-})
- })
- }
+async function newTeam() {
+  try {
+    promptUser().then(input => {
+      promptMan().then(({ office }) => {
+        m = new Manager(input.name, input.id, input.email, office);
+        console.log(`\nNew manager: "${m.name}" has been created.`);
+        promptNew().then(add =>
+          add ? addEmployee() : console.log("generating...")
+        );
+      });
+    });
+  } catch (err) {
+    console.log(err);
+  }
+}
 
+ async function addEmployee() {
+    const role = await promptType();
 
+    switch (role) {
+        case Engineer:
+            
+            break;
+    
+        default:
+            break;
+    }
+   
+}
+ function promptType() {
+    return inquirer.prompt([
+    {
+        type: "checkbox",
+        name: "role",
+        message: "What role with he/she have on your team?",
+        choices: ["Engineer", "intern"]
+    }
+])
+}
+
+ function promptNew() {
+    return inquirer.prompt([
+        {
+        type: "confirm",
+        name: "add",
+        message: "Should we add a new Employee to the team?"
+        },
+      ])
+  }
+  
  function promptMan() {
     return inquirer.prompt([
         {
@@ -44,12 +84,12 @@ function promptUser() {
         {
         type: "number",
         name: "id",
-        message: "Enter your Employee ID #."
+        message: "Enter your Employee ID #:"
         },
         {
         type: "input",
         name: "email",
-        message: "Enter your email address."
+        message: "Enter your email address:"
         },
       ])
  }
